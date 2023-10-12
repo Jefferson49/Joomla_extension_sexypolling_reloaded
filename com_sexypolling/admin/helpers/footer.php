0393f67e916b9cca36c6243655971ce1472a5db3
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2023-10-12 13:09:55 +0200

    Fixes #53: HTML character entities (&nbsp, &copy) are shown in Select a Module Type (adding new site module) in Joomla 4

--- com_sexypolling/admin/helpers/footer.php
+++ com_sexypolling/admin/helpers/footer.php
@@ -24,9 +24,19 @@ defined('_JEXEC') or die('Restircted access');
 <table class="adminlist" style="width: 100%;margin-top: 12px;clear: both;">
 	<tr><td align="center" valign="middle" id="twoglux_ext_td" style="position: relative;height: 70px;">
 	<div id="twoglux_bottom_link">
-		<?php echo JText::_( 'COM_SEXYPOLLING_DEVELOPED_BY' ); ?> 
-		<a href="http://2glux.com" target="_blank">2GLux.com</a>. <?php echo JText::_( 'COM_SEXYPOLLING_EXTENDED_BY' ); ?> 
-		<a href="https://github.com/Jefferson49/" target="_blank">Jefferson49</a>
+		<span>
+			<b><a href="https://github.com/Jefferson49/Joomla_plugin_sexypolling_reloaded" target="_blank">
+				<?php echo JText::_( 'COM_SEXYPOLLING' ); ?>
+			</a></b> - Copyright © 2010 - 2021 <a href="http://2glux.com" target="_blank"><b>2GLux</b></a>. All rights reserved. Copyright © 2022 - 2023 <a href="https://github.com/Jefferson49" target="_blank"><b>Jefferson49</b></a>
+			<br>
+			<a href="https://github.com/Jefferson49/Joomla_plugin_sexypolling_reloaded/releases" target="_blank">
+				<?php echo JText::_( 'COM_SEXYPOLLING_DOWNLOAD' ); ?>
+			</a> | <a href="https://github.com/Jefferson49/Joomla_plugin_sexypolling_reloaded/issues" target="_blank">
+				<?php echo JText::_( 'COM_SEXYPOLLING_SUPPORT' ); ?>
+			</a> | <a href="https://github.com/Jefferson49/Joomla_plugin_sexypolling_reloaded" target="_blank">
+				<?php echo JText::_( 'COM_SEXYPOLLING_DOCUMENTATION' ); ?>
+			</a>
+		</span>
 	</div>
 	</td></tr>
 </table>

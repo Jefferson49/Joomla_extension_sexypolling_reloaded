c73da7ae6b615a1a9dbd331408e67acc5d710bce
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-03-18 06:47:43 +0100

    Joomla 3 compatibility: Changed name of editor button in order to show icon in Joomla 3

--- com_sexypolling/plugins/editor_button/sexypolling.php
+++ com_sexypolling/plugins/editor_button/sexypolling.php
@@ -45,8 +45,7 @@ class PlgButtonSexypolling extends CMSPlugin {
         $button->modal      = true;
         $button->link       = $link;
         $button->text       = Text::_('PLG_EDITORS_XTD_SEXYPOLLING_BUTTON_TEXT');                  
-        $button->name       = $this->_type . '_' . $this->_name;
-        $button->icon       = 'file-add';
+        $button->name       = 'file-add';
         $button->iconSVG    = '<svg viewBox="0 0 32 32" width="24" height="24"><path d="M28 24v-4h-4v4h-4v4h4v4h4v-4h4v-4zM2 2h18v6h6v10h2v-10l-8-8h-20v32h18v-2h-16z"></path></svg>';
         $button->options    = "{handler: 'iframe', size: {x: 990, y: 500}}";		
 		        
